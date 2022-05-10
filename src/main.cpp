75a6f5b30ed784352a223d660a974509a684b9ab
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2022-05-10 18:21:14 +0200

    added encoders

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,11 @@ void setup() {
     cfg.wifi_default_ap = true;
     cfg.wifi_ap_password = "12345678";
 
-    cfg.motor_enable_failsafe = true;
+    cfg.motor_enable_failsafe = false;
     cfg.rbcontroller_app_enable = true;
+    cfg.motor_max_power_pct = 100;
+    cfg.motor_polarity_switch_left = true;
+    cfg.motor_polarity_switch_right = true;
     rkSetup(cfg);
 
     auto builder = Layout.begin();
@@ -48,14 +51,22 @@ void setup() {
     builder.commit();
 
     fmt::print("{}'s roboruka '{}' started!\n", cfg.owner, cfg.name);
-    rkArmMoveTo(145, -45);
-    delay(2000);
+    fmt::print("Battery at {}%, {}mV\n", rkBatteryPercent(), rkBatteryVoltageMv());
+    //rkArmMoveTo(145, -45);
+    //rkArmSetGrabbing(false); // open
+    rkMotorsSetPower(50, 50); 	
+    delay(1000);
+    //rkArmMoveTo(145, 65);
+    //rkArmSetGrabbing(true); // close 
+    rkMotorsSetPower(0, 0); 	
     for (int i = 0; i<10; ++i) {
         // Send text to the android application
         rkControllerSendLog(fmt::format("Tick #{}, battery at {}%, {}mv\n",
             i, rkBatteryPercent(), rkBatteryVoltageMv()));
-        fmt::print("{}\n", i);
-        rkArmMoveTo(i, i);
-        delay(1000);
+        // fmt::print("{}\n", i);
+        int32_t enL = man.motor(0).enc()->value();
+        int32_t enR = man.motor(1).enc()->value();
+        fmt::print("{}: {},  {}\n", i, enL, enR);
+        delay(500);
     }
 }
